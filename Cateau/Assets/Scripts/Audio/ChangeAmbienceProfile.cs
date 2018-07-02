39740f637d7414868e1b11c8bea27f241a86da19
Tezerius <Juppo_1990@hotmail.com>
2018-07-03 00:42:09 +0200

    Audio Changes and new scripts

---
+++ Cateau/Assets/Scripts/Audio/ChangeAmbienceProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAmbienceProfile : SceneTreeObject
+{
+
+    public AmbienceProfile ambienceProfile;
+
+    public override void Continue(int nodeIndex)
+    {
+        //Continued
+    }
+
+    protected override void Initialize()
+    {
+        if (ambienceProfile != null)
+        {
+            ambienceProfile.gameObject.SetActive(true);
+        }
+    }
+
+}
