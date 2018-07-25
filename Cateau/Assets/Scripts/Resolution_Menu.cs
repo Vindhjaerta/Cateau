2bc4bb5915008193db8b747fa6b5cdc030f26a82
sawakanemura <39993010+sawakanemura@users.noreply.github.com>
2018-07-25 13:10:42 +0200

    Change_Resolution Script
    -Script för att ändra resolution
    -Bör läggas på canvasen

---
+++ Cateau/Assets/Scripts/Resolution_Menu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resolution_Menu : MonoBehaviour {
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        resolutions = Screen.resolutions;
+
+        //Clear the existing options in the dropdown menu
+        resolutionDropdown.ClearOptions();
+
+        //Create strings of the resolutions
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        //Add the new options to the dropdown
+        resolutionDropdown.AddOptions(options);
+
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+	}
+	
+    
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+}
