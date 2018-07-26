48c829855eaa91fc99446e449d09ed6c60e73e8e
Tezerius <Juppo_1990@hotmail.com>
2018-07-26 19:28:45 +0200

    Credits & Intro sound based on master

---
+++ Cateau/Assets/Scripts/Audio/SetVolymToMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVolymToMaster : MonoBehaviour
+{
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (GameStateContainer.Instance != null)
+        {
+            audioSource.volume = GameStateContainer.Instance.settings.masterVolume;
+        }
+	}
+	
+}
